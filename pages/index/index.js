2cc1034dd2fcf3ff537769b93ded29f3edb7b891
York Wang <anyone.1984@gmail.com>
2020-10-27 17:07:02 +0800

    按钮动画

--- pages/index/index.js
+++ pages/index/index.js
@@ -2,8 +2,11 @@
 Page({
   data: {
     menu: [{
-      name: '仿抖音翻页效果',
+      name: '仿抖音翻页',
       url: '/pages/playground1/index'
+    }, {
+      name: '开关按钮',
+      url: '/pages/playground2/index'
     }]
   },
 })
---
+++ pages/playground2/index.js
@@ -0,0 +1,49 @@
+// pages/playground2/index.js
+let innerAudioContext, windowHeight, busy = false
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    isOpen: true
+  },
+
+  onLoad(options) {
+    const that = this
+    innerAudioContext = wx.createInnerAudioContext()
+    innerAudioContext.src = '/pages/playground2/button.mp3'
+    innerAudioContext.onPlay(() => {
+      that.setData({
+        isOpen: !this.data.isOpen
+      })
+    })
+
+    const res = wx.getSystemInfoSync()
+    windowHeight = res.windowHeight
+  },
+
+  onUnload() {
+    innerAudioContext.destroy()
+    innerAudioContext = null
+  },
+
+  bindTap() {
+    if(!innerAudioContext.paused) {
+      innerAudioContext.stop()
+    }
+    innerAudioContext.play()
+  },
+
+  bindTouchMove(e) {
+    if(busy) return
+    busy = true
+    const touchY = e.touches[0].pageY
+    if(touchY < windowHeight / 2 - 10 && this.data.isOpen) {
+      this.bindTap()
+    } else if(touchY > windowHeight / 2 + 10 && !this.data.isOpen) {
+      this.bindTap()
+    }
+    busy = false
+  },
+})
